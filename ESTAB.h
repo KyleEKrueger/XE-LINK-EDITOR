dbcefea99382239f14d53c2e891a2887b6ba1a68
brettgal <92054453+brettgal@users.noreply.github.com>
2022-04-01 08:52:48 -0700

    Add files via upload

--- ESTAB.h
+++ ESTAB.h
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+using namespace std;
 
 /*
 * 2-D Array: [Control Section][Variable in Control Section]
@@ -13,21 +18,11 @@ CTRLSC||SYMNAM||ADDRES||LENGTH
 
 //char[argc][2] char[30] 
 
-typedef struct ESTAB{
-    int ctrlSectionEnd;
-    std::vector<ESTABEntry> ESTABtest;
-    //Pass 1: Create new ESTABEntry for Header (isHeader = true, so symname is the name of the ctrlsection; Set the address of the ctrl section) (Starting address = PROGADDR for the first ctrl section, then Starting Address = ctrlSectionEnd)
-    //        Search for EXTDEF
-    //        If EXTDEF create ESTABEntry for each EXTDef symbol with Address = Null, isHeader = False, ctrlSection
-    //        If Symbol defined = ESTABEntry SymName, then calculate address (Address = HeaderAddress + Symbol Address)
-    //        If EOF Set header length to EOF Address - Header Address Increment ctrlsection variable +1
-
-} ESTAB;
-
 class ESTABEntry {
-    bool isHeader;
-    unsigned short int ctrlSection;
-    char symName[6];
+    //All entries in the sym tab should be 6 chars long
+    bool isHeader; // if this is true, then it will be the name of the control section
+    unsigned short int ctrlSection; // int that incriments with each control section
+    char symName[6]; 
     unsigned int Address;
     unsigned int Length;
     
@@ -41,8 +36,23 @@ class ESTABEntry {
         cout << "||";
         cout << setfill('0') << setw(6) << std::hex << Address;
         cout << "||";
-        cout << setfill('0') << setw(6) << std::hex << length;
+        cout << setfill('0') << setw(6) << std::hex << Length;
         cout << std::endl;
     }
 
-}
+};
+
+typedef struct ESTAB{
+    int ctrlSectionEnd; // The address of the end of the last control section
+    std::vector<ESTABEntry> ESTABtest;
+    //Pass 1: Create new ESTABEntry for Header (isHeader = true, so symname is the name of the ctrlsection; Set the address of the ctrl section) (Starting address = PROGADDR for the first ctrl section, then Starting Address = ctrlSectionEnd)
+    //        Search for EXTDEF
+    //        If EXTDEF create ESTABEntry for each EXTDef symbol with Address = Null, isHeader = False, ctrlSection
+    //        If Symbol defined = ESTABEntry SymName, then calculate address (Address = HeaderAddress + Symbol Address)
+    //        If EOF Set header length to EOF Address - Header Address Increment ctrlsection variable +1
+
+    ESTAB(int progADDR){
+        ctrlSectionEnd = progADDR;
+    }
+
+} ESTAB;
