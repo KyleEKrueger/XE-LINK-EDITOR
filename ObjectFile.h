a3c47a5fec7bca033286847c83aa39568aedaa7e
brettgal <92054453+brettgal@users.noreply.github.com>
2022-04-10 16:06:28 -0700

    Add files via upload

--- ObjectFile.h
+++ ObjectFile.h
@@ -1,13 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
 struct ObjectFile{
 public:
-    string headerString;
+string headerString;
 string defineString;
 string referString;
 string textString;
 string modificationString;
 string endString;
 
-public: string generateReferString(string inputString){
+public: 
+
+string generateHeaderString(string programName, int startAddress, int endAddress){
+        string startAdd = to_string(startAddress);
+        int length = endAddress - startAddress;
+        string lengthStr = to_string(length);
+        
+        while (programName.size() < 6){
+                programName += ' ';
+        }
+    
+        string headerString = "H" + programName + startAdd + lengthStr;
+    }
+
+string generateDefineString(string name, string address){
+        
+        while (name.size() < 6){
+                name += ' ';
+        }
+    
+        string headerString = "D" + name + address;
+}
+
+string generateReferString(string inputString){
     //input string processing
     string processingString;
     string processingSubString;
@@ -20,14 +50,29 @@ public: string generateReferString(string inputString){
             k = i-j;
             processingSubString = inputString.substr(j,k); // Starting at J create a substring for k length
             while (processingSubString.size()<6){ //add spaces until the substring is 6 chars in length
-                processingSubString +=' ';
+                processingSubString += ' ';
            }
             processingString += processingSubString;// add substring to the final string
             j = i+1;//start j at the char to the right of the comma
         }
     }
     string returnString = "R"+processingString+"\n";
-    cout<<endl<<returnString<< "|Length: "<<returnString.length()<<endl;
+    cout<<endl<<returnString<< "|Length: " << returnString.length()<<endl;
     return returnString;
+    }
+
+string generateTestString(){
+
 }
+
+string generateModificationString(){
+    
+}
+
+
+string generateEndString(int startAddress){
+        string endString = to_string(startAddress);
+        endString = "E" + endString + "\n";
+        return endString;
+    }
 };
