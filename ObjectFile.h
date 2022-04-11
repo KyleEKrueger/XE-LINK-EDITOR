1c86e9636427c992c0bad540b3721139171175a5
Kyle Krueger <96324612+KyleEKrueger@users.noreply.github.com>
2022-04-10 22:18:29 -0700

    Update ObjectFile.h

--- ObjectFile.h
+++ ObjectFile.h
@@ -3,76 +3,135 @@
 #include <fstream>
 #include <iomanip>
 #include <algorithm>
+#include <sstream>
+
 using namespace std;
 
-struct ObjectFile{
+typedef struct ObjectFile {
 public:
-string headerString;
-string defineString;
-string referString;
-string textString;
-string modificationString;
-string endString;
+    string headerString;
+    string defineString;
+    string referString;
+    string textString;
+    string modificationString;
+    string endString;
 
-public: 
+public:
 
-string generateHeaderString(string programName, int startAddress, int endAddress){
+    string generateHeaderString(string programName, int startAddress, int endAddress) {
         string startAdd = to_string(startAddress);
         int length = endAddress - startAddress;
         string lengthStr = to_string(length);
-        
-        while (programName.size() < 6){
-                programName += ' ';
+
+        while (programName.size() < 6) {
+            programName += ' ';
         }
-    
+
         string headerString = "H" + programName + startAdd + lengthStr;
+        return headerString;
     }
 
-string generateDefineString(string name, string address){
-        
-        while (name.size() < 6){
-                name += ' ';
+    string generateDefineString(string name, string address) {
+
+        while (name.size() < 6) {
+            name += ' ';
         }
-    
+
         string headerString = "D" + name + address;
-}
+    }
 
-string generateReferString(string inputString){
-    //input string processing
-    string processingString;
-    string processingSubString;
-    unsigned short int j=0; // This is the beginning of a substring [j, ',']
-    unsigned short int k=0; // K is the distance between substrings
-    for(int i=0; i<=inputString.size();i++){
-        //Grab sub string from j (end of last sub string) to i-1 (string before the last char / comma is)
-        if (inputString[i] == ','|| i == inputString.length()){
-            //Find the length between j and i
-            k = i-j;
-            processingSubString = inputString.substr(j,k); // Starting at J create a substring for k length
-            while (processingSubString.size()<6){ //add spaces until the substring is 6 chars in length
-                processingSubString += ' ';
-           }
-            processingString += processingSubString;// add substring to the final string
-            j = i+1;//start j at the char to the right of the comma
+    string generateReferString(string inputString) {
+        //input string processing
+        string processingString;
+        string processingSubString;
+        unsigned short int j = 0; // This is the beginning of a substring [j, ',']
+        unsigned short int k = 0; // K is the distance between substrings
+        for (int i = 0; i <= inputString.size(); i++) {
+            //Grab sub string from j (end of last sub string) to i-1 (string before the last char / comma is)
+            if (inputString[i] == ',' || i == inputString.length()) {
+                //Find the length between j and i
+                k = i - j;
+                processingSubString = inputString.substr(j, k); // Starting at J create a substring for k length
+                while (processingSubString.size() < 6) { //add spaces until the substring is 6 chars in length
+                    processingSubString += ' ';
+                }
+                processingString += processingSubString;// add substring to the final string
+                j = i + 1;//start j at the char to the right of the comma
+            }
         }
+        string returnString = "R" + processingString + "\n";
+        cout << endl << returnString << "|Length: " << returnString.length() << endl;
+        return returnString;
     }
-    string returnString = "R"+processingString+"\n";
-    cout<<endl<<returnString<< "|Length: " << returnString.length()<<endl;
-    return returnString;
+
+    string generateTestString() {
+        return "GenerateTestString()";
     }
 
-string generateTestString(){
-
-}
-
-string generateModificationString(){
-    
-}
+    string generateModificationString() {
+        return "GenerateModificationString()";
+    }
 
 
-string generateEndString(int startAddress){
+    string generateEndString(int startAddress) {
         string endString = to_string(startAddress);
         endString = "E" + endString + "\n";
         return endString;
     }
-};
+
+
+}ObjectFile;
+
+typedef class TextRecord {
+public:
+    string textRecordLine; //0-68 chars for total text record, 60 chars max of instructions
+    string recordStartingAdd;
+    int textRecordLength;
+
+    int getTextRecordLength(){
+        int lineLength = textRecordLine.length();
+        textRecordLength = lineLength/2; // in Bytes
+        return textRecordLength;
+    }
+
+    //Generates the text record file and resets the Text record variables
+    string generateTextRecord() {
+        string outputString = "T";
+        string intermString;
+        stringstream ss;
+        ss << setw(6) << setfill('0') << right << recordStartingAdd;
+        ss >> intermString;
+        ss.clear();
+        outputString += intermString;
+        intermString = "";
+        ss<<std::hex<<setw(6)<<setfill('0')<<right<<recordStartingAdd;
+        ss>>intermString;
+        outputString+=intermString;
+        intermString = "";
+        ss<<std::hex<<setw(2)<<setfill('0')<<right<<getTextRecordLength();
+        ss>>intermString;
+        outputString+=intermString;
+        intermString = "";
+        outputString+=textRecordLine;
+        cout<<outputString<<endl;//eventually add to something else
+
+    }
+
+    //Adds an opCode to a text record, generates text record if text record has reached maximum length
+    void addTextRecordInstruction(string opCode) {
+        if (textRecordLine.length() + opCode.length() > 60) {
+            generateTextRecord();
+            addTextRecordInstruction(opCode);
+        } else {
+            textRecordLine += opCode;
+        }
+    }
+
+    string stringToStringDecToHex(string inputString){
+        stringstream ss;
+        string intermString;
+        ss<<std::hex<<stoi(inputString);
+        ss>>intermString;
+        return intermString;
+    }
+} TextRecord;
